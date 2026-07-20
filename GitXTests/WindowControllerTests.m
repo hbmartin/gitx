e4d72e551b4b57285ea3159883f732754546de02
Harold Martin <harold.martin@gmail.com>
2026-07-19 20:31:25 -0700

    Cover repository settings path fallback

--- GitXTests/WindowControllerTests.m
+++ GitXTests/WindowControllerTests.m
@@ -144,6 +144,10 @@
 @interface PBWindowRepositoryWithoutGitURLs : PBGitRepository
 @end
 
+@interface PBWindowRepositoryWithGitURLOnly : PBGitRepository
+@property (nonatomic, strong) NSURL *testGitURL;
+@end
+
 @interface PBWelcomeWindowController : NSWindowController
 + (instancetype)shared;
 - (void)show;
@@ -237,6 +241,50 @@
 - (nullable NSURL *)workingDirectoryURL
 {
 	return nil;
+}
+
+@end
+
+@implementation PBWindowRepositoryWithGitURLOnly
+
+- (nullable NSString *)outputOfTaskWithArguments:(nullable NSArray *)arguments error:(NSError **)error
+{
+	return @".git/common\n";
+}
+
+- (nullable NSURL *)gitURL
+{
+	return self.testGitURL;
+}
+
+- (nullable NSURL *)workingDirectoryURL
+{
+	return nil;
+}
+
+@end
+
+@interface RepositoryUISettingsTests : XCTestCase
+@end
+
+@implementation RepositoryUISettingsTests
+
+- (void)testRepositoryUISettingsUsesGitURLForRelativeCommonDirectoryWithoutWorkingDirectory
+{
+	id originalSettings = [NSUserDefaults.standardUserDefaults objectForKey:@"PBRepositoryUISettings"];
+	PBWindowRepositoryWithGitURLOnly *repository = [PBWindowRepositoryWithGitURLOnly new];
+	repository.testGitURL = [NSURL fileURLWithPath:@"/tmp/GitXGitURLOnly" isDirectory:YES];
+	PBRepositoryUISettings *settings = [[PBRepositoryUISettings alloc] initWithRepository:repository];
+
+	settings.pushAfterCommit = YES;
+	NSURL *commonDirectory = [repository.testGitURL URLByAppendingPathComponent:@".git/common" isDirectory:YES];
+	NSString *repositoryKey = commonDirectory.standardizedURL.URLByResolvingSymlinksInPath.path;
+	NSDictionary *allSettings = [NSUserDefaults.standardUserDefaults dictionaryForKey:@"PBRepositoryUISettings"];
+	XCTAssertEqualObjects(allSettings[repositoryKey][@"pushAfterCommit"], @YES);
+	if (originalSettings)
+		[NSUserDefaults.standardUserDefaults setObject:originalSettings forKey:@"PBRepositoryUISettings"];
+	else
+		[NSUserDefaults.standardUserDefaults removeObjectForKey:@"PBRepositoryUISettings"];
 }
 
 @end
